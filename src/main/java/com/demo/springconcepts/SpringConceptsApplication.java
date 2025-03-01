6adb56626f8a5be02bc4873e229f2888dbbfc419
Diksha Trivedi <diksha.trivedi2122@gmail.com>
2025-03-01 20:02:59 +0530

    Dependency injection using employee and department bean

--- src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -1,5 +1,6 @@
 package com.demo.springconcepts;
 
+import com.demo.springconcepts.components.EmployeeBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.demo.springconcepts.components.DemoBean;
@@ -17,7 +18,11 @@ public class SpringConceptsApplication {
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
 		logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
 		logger.debug("\n*** Example Using @Autowire annotation on property *");
-		}
+		EmployeeBean employeeBean=context.getBean(EmployeeBean.class);
+		employeeBean.setEid(104);
+		employeeBean.setEname("Spring Framework Guru");
+		employeeBean.showEmployeeDetails();
+	}
 }
 
 
